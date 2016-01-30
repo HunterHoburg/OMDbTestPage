a18f0d85c09864cafd1d37698c0a5dbc5b0364f8
hhoburg <hhoburg@gmail.com>
2016-01-29 23:53:41 -0700

    about to try including rotten tomatoes ratings and plot descriptions in the http request, might really mess things up

--- js/controllers.js
+++ js/controllers.js
@@ -4,13 +4,23 @@ app.controller('getMovies', ['$http', function($http){
   vm.retrieve = function(title) {
     titleParse(title);
     vm.movieArr = [];
-    $http.get('http://www.omdbapi.com/?s='+title+'&r=json').success(function(data){
+    $http.get('http://www.omdbapi.com/?s='+title+'&tomatoes=true&r=json').success(function(data){
+      console.log(data);
       for (var j = 0; j < data.Search.length - 1; j++) {
         var newMovie = {};
         newMovie = data.Search[j];
-        console.log(newMovie);
+        // console.log(newMovie);
         vm.movieArr.push(newMovie);
       }
     })
   };
+}])
+
+//TRYING TO CREATE A CONTROLLER TO SHOW OR HIDE THE EXTRA MOVIE INFO ON CLICK OR ON THE BACK BUTTON CLICK
+
+app.controller('popUp', ['$http', function($http) {
+  var vm = this;
+  vm.retrieve = function(title) {
+    titleParse(title);
+  }
 }])
