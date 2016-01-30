d3d6d324b6e56094284a33ab996acd048f4084b5
hhoburg <hhoburg@gmail.com>
2016-01-30 01:07:21 -0700

    got detailed plot information to come up, but it appears on every single entry that is pulled up, so imma fix that

--- js/controllers.js
+++ js/controllers.js
@@ -5,11 +5,10 @@ app.controller('getMovies', ['$http', function($http){
     titleParse(title);
     // vm.movieArr = [];
     $http.get('http://www.omdbapi.com/?s='+title+'&r=json').success(function(data){
-      console.log(data);
+      // console.log(data);
       for (var j = 0; j < data.Search.length - 1; j++) {
         var newMovie = {};
         newMovie = data.Search[j];
-        // console.log(newMovie);
         vm.movieArr.push(newMovie);
       }
     })
@@ -20,10 +19,21 @@ app.controller('getMovies', ['$http', function($http){
 
 app.controller('popUp', ['$http', function($http) {
   var vm = this;
+  vm.selectedIndex = 0;
+  vm.movieArr = [];
   vm.retrieve = function(title) {
+    if (vm.selectedIndex == 1) {
+    vm.selectedIndex = 0;
+  } else if (vm.selectedIndex == 0){
+    vm.selectedIndex = 1;
+  }
+    vm.movieArr = [];
     titleParse(title);
     $http.get('http://www.omdbapi.com/?t='+title+'&plot=full&tomatoes=true&r=json').success(function(data){
+      var newMovie = {};
       console.log(data);
+      newMovie = data
+      vm.movieArr.push(newMovie);
     })
   }
 }])
